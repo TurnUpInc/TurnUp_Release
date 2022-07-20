d8d25eb493779349462c965dbf562095e1a8664b
Jayant <hereisjayant@gmail.com>
2022-07-19 19:17:59 -0700

    fixed codacy issues #4

--- Testing/BackendTest/tests/UserHandler.test.js
+++ Testing/BackendTest/tests/UserHandler.test.js
@@ -126,7 +126,7 @@ it('Gets liked events of the user', async () => {
     }
 
   axios.get.mockResolvedValue(
-    {data: data,
+    {data,
       status: 200}
   );
 
@@ -165,7 +165,7 @@ it('Gets liked locations of the user', async () => {
     }
 
   axios.get.mockResolvedValue(
-    {data: data,
+    {data,
       status: 200}
   );
 
