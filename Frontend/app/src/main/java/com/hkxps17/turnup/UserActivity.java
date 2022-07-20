38762f85992b66ab646acf2bebecfec6684c930a
Hiren Khurana <65192069+hirenkhurana@users.noreply.github.com>
2022-07-20 08:00:41 -0700

    NPath complexity fix v2

--- Frontend/app/src/main/java/com/hkxps17/turnup/UserActivity.java
+++ Frontend/app/src/main/java/com/hkxps17/turnup/UserActivity.java
@@ -4,11 +4,11 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
-import android.text.Layout;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -17,7 +17,6 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ImageButton;
-import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.Spinner;
 import android.widget.TextView;
@@ -34,7 +33,6 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashSet;
@@ -48,14 +46,23 @@ public class UserActivity extends AppCompatActivity {
     ImageButton done;
     EditText name;
 
-    Spinner color1, color2, color3;
+    Spinner color1;
+    Spinner color2;
+    Spinner color3;
     String[] colors = {"Green", "Blue", "Pink"};
     String[] prefs = {"Green", "Blue", "Pink"};
     String[] userprf = {"Green", "Blue", "Pink"};
 
     String emailID = "";
 
-    ArrayList<String> EventTitles = new ArrayList<>(), EventLocations = new ArrayList<>(), EventDates = new ArrayList<>(), EventRatings = new ArrayList<>(), EventCategories = new ArrayList<>(), EventDescriptions = new ArrayList<>(), EventImages = new ArrayList<>(), likedBy = new ArrayList<>();
+    ArrayList<String> EventTitles = new ArrayList<>();
+    ArrayList<String> EventLocations = new ArrayList<>();
+    ArrayList<String> EventDates = new ArrayList<>();
+    ArrayList<String> EventRatings = new ArrayList<>();
+    ArrayList<String> EventCategories = new ArrayList<>();
+    ArrayList<String> EventDescriptions = new ArrayList<>();
+    ArrayList<String> EventImages = new ArrayList<>();
+    ArrayList<String> likedBy = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -74,6 +81,133 @@ public class UserActivity extends AppCompatActivity {
             findViewById(R.id.manageview).setVisibility(View.INVISIBLE);
         }
 
+        StringRequest eventsReq = apiCall();
+        RequestQueue queue = Volley.newRequestQueue(UserActivity.this);
+        queue.add(eventsReq);
+
+        Log.d("EMAILUSER", emailID);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent i = new Intent(UserActivity.this, AddEventActivity.class);
+                i.putExtra("Manage", "YES");
+                i.putExtra("EventTitle", EventTitles.get(position));
+                i.putExtra("EventLocation", EventLocations.get(position));
+                i.putExtra("EventDate", EventDates.get(position));
+                i.putExtra("EventDescription", EventDescriptions.get(position));
+                i.putExtra("EventImage", EventImages.get(position));
+                i.putExtra("EventRating", EventRatings.get(position));
+                i.putExtra("EventCategory", EventCategories.get(position));
+                startActivity(i);
+            }
+        });
+
+        name = findViewById(R.id.your_name);
+        name.setText(emailID);
+
+        setColor1();
+
+        setColor2();
+
+        setColor3();
+
+
+        done = findViewById(R.id.done_button);
+        done.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                List<String> prefList = Arrays.asList(prefs);
+                Set<String> tasksSet = new HashSet<String>(prefList);
+                PreferenceManager.getDefaultSharedPreferences(UserActivity.this)
+                        .edit()
+                        .putStringSet("tasks_set", tasksSet)
+                        .commit();
+
+                Log.d("TEST", prefList.toString() + "!!");
+                Intent list = new Intent(UserActivity.this, EventListActivity.class);
+                startActivity(list);
+            }
+        });
+    }
+
+    private void setColor3() {
+        color3 = findViewById(R.id.color3_spinner);
+        ArrayAdapter<String> adapter3 = new ArrayAdapter<String>(UserActivity.this, android.R.layout.simple_spinner_item, colors);
+        adapter3.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        color3.setAdapter(adapter3);
+
+        if (Objects.equals(userprf[2], "Green")) color3.setSelection(0);
+        else if (Objects.equals(userprf[2], "Pink")) color3.setSelection(2);
+        else color3.setSelection(1);
+
+
+        color3.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                if (i == 0) prefs[2] = "LikeGreen";
+                else if (i == 1) prefs[2] = "LikeBlue";
+                else prefs[2] = "LikePink";
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+                Log.d("Nothing", "nothing selected");
+            }
+        });
+    }
+
+    private void setColor2() {
+        color2 = findViewById(R.id.color2_spinner);
+        ArrayAdapter<String> adapter2 = new ArrayAdapter<String>(UserActivity.this, android.R.layout.simple_spinner_item, colors);
+        adapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        color2.setAdapter(adapter2);
+
+        if (Objects.equals(userprf[1], "Green")) color2.setSelection(0);
+        else if (Objects.equals(userprf[1], "Pink")) color2.setSelection(2);
+        else color2.setSelection(1);
+
+        color2.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                if (i == 0) prefs[1] = "ManageGreen";
+                else if (i == 1) prefs[1] = "ManageBlue";
+                else prefs[1] = "ManagePink";
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+                Log.d("Nothing", "nothing selected");
+            }
+        });
+    }
+
+    private void setColor1() {
+        color1 = findViewById(R.id.color1_spinner);
+        ArrayAdapter<String> adapter1 = new ArrayAdapter<String>(UserActivity.this, android.R.layout.simple_spinner_item, colors);
+        adapter1.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        color1.setAdapter(adapter1);
+
+        if (Objects.equals(userprf[0], "Blue")) color1.setSelection(1);
+        else if (Objects.equals(userprf[0], "Pink")) color1.setSelection(2);
+        else color1.setSelection(0);
+
+
+        color1.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                if (i == 0) prefs[0] = "ListGreen";
+                else if (i == 1) prefs[0] = "ListBlue";
+                else prefs[0] = "ListPink";
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+                Log.d("Nothing", "nothing selected");
+            }
+        });
+    }
+
+    private StringRequest apiCall() {
         String getAllMyEventsApiUrl = "http://20.122.91.139:8081/myevents/"+emailID;
         StringRequest eventsReq = new StringRequest(Request.Method.GET, getAllMyEventsApiUrl, new Response.Listener<String>() {
             @Override
@@ -114,117 +248,7 @@ public class UserActivity extends AppCompatActivity {
                 Toast.makeText(UserActivity.this, "Loading", Toast.LENGTH_LONG).show();
             }
         });
-        RequestQueue queue = Volley.newRequestQueue(UserActivity.this);
-        queue.add(eventsReq);
-
-        Log.d("EMAILUSER", emailID);
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Intent i = new Intent(UserActivity.this, AddEventActivity.class);
-                i.putExtra("Manage", "YES");
-                i.putExtra("EventTitle", EventTitles.get(position));
-                i.putExtra("EventLocation", EventLocations.get(position));
-                i.putExtra("EventDate", EventDates.get(position));
-                i.putExtra("EventDescription", EventDescriptions.get(position));
-                i.putExtra("EventImage", EventImages.get(position));
-                i.putExtra("EventRating", EventRatings.get(position));
-                i.putExtra("EventCategory", EventCategories.get(position));
-                startActivity(i);
-            }
-        });
-
-        name = findViewById(R.id.your_name);
-        name.setText(emailID);
-
-        color1 = findViewById(R.id.color1_spinner);
-        ArrayAdapter<String> adapter1 = new ArrayAdapter<String>(UserActivity.this, android.R.layout.simple_spinner_item, colors);
-        adapter1.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        color1.setAdapter(adapter1);
-
-        if (userprf[0] == "Blue") color1.setSelection(1);
-        else if (userprf[0] == "Pink") color1.setSelection(2);
-        else color1.setSelection(0);
-
-
-        color1.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
-                if (i == 0) prefs[0] = "ListGreen";
-                else if (i == 1) prefs[0] = "ListBlue";
-                else prefs[0] = "ListPink";
-            }
-
-            @Override
-            public void onNothingSelected(AdapterView<?> adapterView) {
-
-            }
-        });
-
-        color2 = findViewById(R.id.color2_spinner);
-        ArrayAdapter<String> adapter2 = new ArrayAdapter<String>(UserActivity.this, android.R.layout.simple_spinner_item, colors);
-        adapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        color2.setAdapter(adapter2);
-
-        if (userprf[1] == "Green") color2.setSelection(0);
-        else if (userprf[1] == "Pink") color2.setSelection(2);
-        else color2.setSelection(1);
-
-        color2.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
-                if (i == 0) prefs[1] = "ManageGreen";
-                else if (i == 1) prefs[1] = "ManageBlue";
-                else prefs[1] = "ManagePink";
-            }
-
-            @Override
-            public void onNothingSelected(AdapterView<?> adapterView) {
-
-            }
-        });
-
-        color3 = findViewById(R.id.color3_spinner);
-        ArrayAdapter<String> adapter3 = new ArrayAdapter<String>(UserActivity.this, android.R.layout.simple_spinner_item, colors);
-        adapter3.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        color3.setAdapter(adapter3);
-
-        if (Objects.equals(userprf[2], "Green")) color3.setSelection(0);
-        else if (Objects.equals(userprf[2], "Pink")) color3.setSelection(2);
-        else color3.setSelection(1);
-
-
-        color3.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
-                if (i == 0) prefs[2] = "LikeGreen";
-                else if (i == 1) prefs[2] = "LikeBlue";
-                else prefs[2] = "LikePink";
-            }
-
-            @Override
-            public void onNothingSelected(AdapterView<?> adapterView) {
-
-            }
-        });
-
-
-        done = findViewById(R.id.done_button);
-        done.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                List<String> prefList = Arrays.asList(prefs);
-                Set<String> tasksSet = new HashSet<String>(prefList);
-                PreferenceManager.getDefaultSharedPreferences(UserActivity.this)
-                        .edit()
-                        .putStringSet("tasks_set", tasksSet)
-                        .commit();
-
-                Log.d("TEST", prefList.toString() + "!!");
-                Intent list = new Intent(UserActivity.this, EventListActivity.class);
-                startActivity(list);
-            }
-        });
+        return eventsReq;
     }
 
     public class ManageEventAdapter extends ArrayAdapter<String> {
@@ -241,11 +265,12 @@ public class UserActivity extends AppCompatActivity {
             this.eLocations = locations;
         }
 
+        @SuppressLint("UseCompatLoadingForDrawables")
         @NonNull
         @Override
         public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
             LayoutInflater layoutInflater = (LayoutInflater) context.getApplicationContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-            View eventItem = layoutInflater.inflate(R.layout.manage_event_item, parent, false);
+            @SuppressLint("ViewHolder") View eventItem = layoutInflater.inflate(R.layout.manage_event_item, parent, false);
             TextView title = eventItem.findViewById(R.id.EventTitle);
             TextView date = eventItem.findViewById(R.id.EventDate);
             TextView loc = eventItem.findViewById(R.id.EventLocation);
