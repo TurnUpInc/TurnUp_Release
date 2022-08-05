bb8265edc3c0dae45bb802d8349a328ff8621795
Rushil Punchhi <rushilxo5@gmail.com>
2022-08-06 02:02:59 +0530

    push notification

--- Frontend/app/src/main/java/com/hkxps17/turnup/ChatActivity.java
+++ Frontend/app/src/main/java/com/hkxps17/turnup/ChatActivity.java
@@ -2,13 +2,19 @@ package com.hkxps17.turnup;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.annotation.SuppressLint;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Intent;
 import android.graphics.drawable.Drawable;
 import android.graphics.drawable.GradientDrawable;
+import android.os.Build;
 import android.os.Bundle;
 import androidx.preference.PreferenceManager;
 import android.text.TextUtils;
@@ -40,6 +46,7 @@ import io.socket.emitter.Emitter;
 
 public class ChatActivity extends AppCompatActivity implements View.OnClickListener {
 
+    private static final String CHANNEL_ID = "1";
     private EditText mEditTextChatBox;
     private ChatAdapter chatAdapter;
     private ArrayList<String> mChatList = new ArrayList<>();
@@ -155,6 +162,22 @@ public class ChatActivity extends AppCompatActivity implements View.OnClickListe
                     String username = data.optString("username");
                     String time = data.optString("time");
                     mChatList.add(username + " (" + time + "): \n" + message);
+                    createNotificationChannel();
+                    // Create an explicit intent for an Activity in your app
+                    Intent intent = new Intent(ChatActivity.this, ChatActivity.class);
+                    intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                    PendingIntent pendingIntent = PendingIntent.getActivity(ChatActivity.this, 0, intent, PendingIntent.FLAG_IMMUTABLE);
+                    NotificationCompat.Builder builder = new NotificationCompat.Builder(ChatActivity.this, CHANNEL_ID)
+                            .setSmallIcon(R.drawable.ic_message)
+                            .setContentTitle("My notification")
+                            .setContentText("Hello World!")
+                            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                            // Set the intent that will fire when the user taps the notification
+                            .setContentIntent(pendingIntent)
+                            .setAutoCancel(true);
+                    NotificationManagerCompat notificationManager = NotificationManagerCompat.from(ChatActivity.this);
+                    //notificationId is a unique int for each notification that you must define
+                    notificationManager.notify(0, builder.build());
                     RecyclerView mRecyclerViewChat = findViewById(R.id.rv_chat);
                     mRecyclerViewChat.scrollToPosition(mChatList.size() - 1);
                     if (chatAdapter != null)
@@ -217,6 +240,22 @@ public class ChatActivity extends AppCompatActivity implements View.OnClickListe
                     }
                 }
             }
+        }
+    }
+
+    private void createNotificationChannel() {
+        // Create the NotificationChannel, but only on API 26+ because
+        // the NotificationChannel class is new and not in the support library
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            CharSequence name = "MSG";
+            String description = "message channel";
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+            NotificationChannel channel = new NotificationChannel(CHANNEL_ID, name, importance);
+            channel.setDescription(description);
+            // Register the channel with the system; you can't change the importance
+            // or other notification behaviors after this
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
         }
     }
 }
